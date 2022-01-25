6b504b811382a0aaf162fa768148a1648b657ca6
Francisco Maribao <franciscomaribao98@gmail.com>
2022-01-25 16:05:52 +0800

    fix sql

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,13 +20,13 @@ class PageController extends Controller
     	if(!Auth::check()){
     		return redirect('/login');
     	}
-        // (SELECT SUM(d_items.price*d_bounds.qty) FROM item_bounds AS d_bounds INNER JOIN items AS d_items on d_bounds.item = d_items.id WHERE CAST(d_bounds.created_at AS DATE) = CAST('".date('Y-m-d')."' AS DATE) AND d_bounds.type='outbound') AS daily
         $revenue = DB::table('item_bounds')
         ->leftJoin('items', 'item_bounds.item', '=', 'items.id')
         ->select(DB::raw("
             SUM(items.price*item_bounds.qty) AS total,
-            (SELECT SUM(m_items.price*m_bounds.qty) FROM item_bounds AS m_bounds INNER JOIN items AS m_items on m_bounds.item = m_items.id WHERE MONTH(m_bounds.created_at) = '".date('m')."' AND m_bounds.type='outbound') AS monthly,
-            (SELECT SUM(w_items.price*w_bounds.qty) FROM item_bounds AS w_bounds INNER JOIN items AS w_items on w_bounds.item = w_items.id WHERE WEEK(w_bounds.created_at) = '".abs(date('W'))."' AND w_bounds.type='outbound') AS weekly  
+            (SELECT CAST(SUM(m_items.price*m_bounds.qty) AS FLOAT) FROM item_bounds AS m_bounds INNER JOIN items AS m_items on m_bounds.item = m_items.id WHERE MONTH(m_bounds.created_at) = '".date('m')."' AND m_bounds.type='outbound') AS monthly,
+            (SELECT CAST(SUM(w_items.price*w_bounds.qty) AS FLOAT) FROM item_bounds AS w_bounds INNER JOIN items AS w_items on w_bounds.item = w_items.id WHERE WEEK(w_bounds.created_at) = '".abs(date('W'))."' AND w_bounds.type='outbound') AS weekly,
+            (SELECT CAST(SUM(d_items.price*d_bounds.qty) AS FLOAT) FROM item_bounds AS d_bounds INNER JOIN items AS d_items on d_bounds.item = d_items.id WHERE CAST(d_bounds.created_at AS DATE) = CAST('".date('Y-m-d')."' AS DATE) AND d_bounds.type='outbound') AS daily
         "))
         ->whereRaw("item_bounds.type = 'outbound'")
         ->first();
